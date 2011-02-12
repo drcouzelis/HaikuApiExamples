c92fbd1fb7d654288adbc454ff45443246ec4610
drcouzelis <drcouzelis@182f5939-4430-4b05-87ca-e5b42643dff2>
2011-02-12 04:55:18 +0000

    Fixed tabs.
    git-svn-id: http://svn.osdrawer.net/haiku-api-examples@30 182f5939-4430-4b05-87ca-e5b42643dff2

--- FallLeaves/FallLeaves.h
+++ FallLeaves/FallLeaves.h
@@ -27,38 +27,38 @@ typedef class Leaf;
 class FallLeaves : public BScreenSaver
 {
 public:
-					FallLeaves(BMessage* archive, image_id thisImage);
-					~FallLeaves();
+							FallLeaves(BMessage* archive, image_id thisImage);
+							~FallLeaves();
 	
-	void			StartConfig(BView* configView);
-	status_t		StartSaver(BView* view, bool preview);
-	status_t		SaveState(BMessage* into) const;
+	void					StartConfig(BView* configView);
+	status_t				StartSaver(BView* view, bool preview);
+	status_t				SaveState(BMessage* into) const;
 	
-	void			Draw(BView* view, int32 frame);
+	void					Draw(BView* view, int32 frame);
 	
-	void			SetAmount(int32 amount);
-	void			SetSpeed(int32 speed);
+	void					SetAmount(int32 amount);
+	void					SetSpeed(int32 speed);
 private:
-	Leaf*			_CreateLeaf(BView* view, bool above);
-	BBitmap*		_RandomBitmap(int32 size);
+	Leaf*					_CreateLeaf(BView* view, bool above);
+	BBitmap*				_RandomBitmap(int32 size);
 	
 	BObjectList<Leaf>*		fLeaves;
 	
-	int32			fSize;
-						// The size of the biggest possible leaf
+	int32					fSize;
+								// The size of the biggest possible leaf
 	
-	int32			fAmount;
-						// The amount of leaves on the screen
-	int32			fSpeed;
-						// The speed of the fastest leaf
+	int32					fAmount;
+								// The amount of leaves on the screen
+	int32					fSpeed;
+								// The speed of the fastest leaf
 	
-	BBitmap*		fBackBitmap;
-	BView*			fBackView;
-						// For double buffering,
-						// used to reduce flicker
+	BBitmap*				fBackBitmap;
+	BView*					fBackView;
+								// For double buffering,
+								// used to reduce flicker
 	
-	bool			fZUsed[101];
-						// Used to give each leaf a unique Z depth
+	bool					fZUsed[101];
+								// Used to give each leaf a unique Z depth
 };
 
 

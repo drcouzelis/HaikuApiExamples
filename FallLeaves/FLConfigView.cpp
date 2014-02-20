05da4abea98561e57ad32ced8ec19a3dd9d78e83
David Couzelis <drcouzelis@gmail.com>
2014-02-19 20:27:08 -0500

    Updated the FallLeaves screensaver to use the newer translation hooks.

--- FallLeaves/FLConfigView.cpp
+++ FallLeaves/FLConfigView.cpp
@@ -12,8 +12,8 @@
 
 
 // For language translations
-#undef B_TRANSLATE_CONTEXT
-#define B_TRANSLATE_CONTEXT "FallLeaves"
+#undef B_TRANSLATION_CONTEXT
+#define B_TRANSLATION_CONTEXT "FallLeaves"
 
 
 enum {
